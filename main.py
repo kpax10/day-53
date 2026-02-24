276d759e87ed334a0fb8f7b3e6cec459b5e4dff7
kyle <notxap55@yahoo.com>
2026-02-23 16:11:35 -0800

    prices extracted correctly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
 GOOGLE_FORM_LINK = 'https://docs.google.com/forms/d/e/1FAIpQLSep-Da6bRUT9yxqkIdZQ5mx1_V10lDX5YxZkrHQNodWT9NArw/viewform?usp=sharing&ouid=115047315612100414139'
 ZILLOW_CLONE_SITE = 'https://appbrewery.github.io/Zillow-Clone/'
@@ -7,7 +8,9 @@ ZILLOW_CLONE_SITE = 'https://appbrewery.github.io/Zillow-Clone/'
 response = requests.get(ZILLOW_CLONE_SITE, timeout=5)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-link_html = soup.find_all('a', class_='property-card-link', href=True)
-links_text = [link['href'] for link in link_html]
+links_html = soup.find_all('a', class_='property-card-link', href=True)
+links_text = [link['href'] for link in links_html]
 
+prices_html = soup.find_all(attrs={'data-test':'property-card-price'})
+prices_text = [re.split(r"[\/+]", price.text)[0] for price in prices_html]
 
